6d0874e3ddaaa3d8cb4b403e4ccab4322a60101e
Patrykf03 <61631089+patrykf03@users.noreply.github.com>
2023-05-21 16:21:31 +0100

    add missing files to make kb2040 work

---
+++ micropython/modules/micropython-kb2040.cmake
@@ -0,0 +1,32 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+# Essential
+include(pimoroni_i2c/micropython)
+include(pimoroni_bus/micropython)
+
+# Pico Graphics Essential
+include(picographics/micropython)
+
+# Sensors & Breakouts
+include(micropython-common-breakouts)
+
+# LEDs & Matrices
+include(plasma/micropython)
+
+include(micropython-common-ulab)
+enable_ulab()
+
+# Scroll
+include(pico_scroll/miicropython)
+
+include(modules_py/modules_py)
+
+# C++ Magic Memory
+include(cppmem/micropython)
